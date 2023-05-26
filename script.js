d7b87eb11b7e96648b30941d7a8d9b2a4c21b559
BaranikumarBala <127426534+BaranikumarBala@users.noreply.github.com>
2023-05-27 00:41:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var http = new XMLHttpRequest();
 var arr = [];
-http.open("GET","http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",true);
+http.open("GET"," https://admin-panel-data-edyoda-sourav.vercel.app/admin/data",true);
 http.onreadystatechange = 
 function()
 {
